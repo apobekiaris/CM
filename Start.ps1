80ae160807d80f44f3ea4e72109a171a90ab9a39
apobekiaris <apostolis.bekiaris@gmail.com>
2023-11-07 22:24:49 +0200

    add cache nuget

--- Start.ps1
+++ Start.ps1
@@ -5,24 +5,23 @@ param(
     $DXApiFeed=$env:DxFeed
 )
 
-$yaml = @"
-- Name: XpandPwsh
-  Version: 1.221.0.7
-"@
-& "$PSScriptRoot\Install-Module.ps1" $yaml
-$directory ="$env:TEMP\CR"
-if (Test-Path $directory){
-    Remove-Item $directory -Recurse -Force
-}
-New-Item $directory -ItemType Directory -Force 
-Set-Location $directory
+# $yaml = @"
+# - Name: XpandPwsh
+#   Version: 1.221.0.7
+# "@
+# & "$PSScriptRoot\Install-Module.ps1" $yaml
+# $directory ="$env:TEMP\CM"
+# if (Test-Path $directory){
+#     Remove-Item $directory -Recurse -Force
+# }
+# New-Item $directory -ItemType Directory -Force 
+# Set-Location $directory
 $url = "https://$GithubUserName`:$GithubPass@github.com/apobekiaris/CryptoManager2.git"
 git clone $url
 cd .\CryptoManager2
 
-
 dotnet nuget add source $DXApiFeed --name DX
 dotnet nuget add source https://xpandnugetserver.azurewebsites.net/nuget --name Xpand
-Start-Build -Configuration DEBUG
+dotnet build --configuration DEBUG
 
 & sqllocaldb start mssqllocaldb
