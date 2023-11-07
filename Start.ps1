ce4b2f6efdd4206cd198a15c6f3c238c36876cee
apobekiaris <apostolis.bekiaris@gmail.com>
2023-11-07 23:16:48 +0200

    Tests

--- Start.ps1
+++ Start.ps1
@@ -1,7 +1,6 @@
 param(
-    $GithubUserName ="apobekiaris",
-    $GithubPass=$env:apobekiarisToken,
-    $GitUserEmail,
+    $GithubUserName =$env:CM_GitHub_UserName,
+    $GithubPass=$env:cmToken,
     $DXApiFeed=$env:DxFeed
 )
 $url = "https://$GithubUserName`:$GithubPass@github.com/apobekiaris/CryptoManager2.git"
@@ -10,6 +9,10 @@ cd .\CryptoManager2
 
 dotnet nuget add source $DXApiFeed --name DX
 dotnet nuget add source https://xpandnugetserver.azurewebsites.net/nuget --name Xpand
-dotnet build --configuration DEBUG
+dotnet build --configuration Debug
+
+if ($LASTEXITCODE -ne 0) {
+  exit $LASTEXITCODE
+}
 
 & sqllocaldb start mssqllocaldb
