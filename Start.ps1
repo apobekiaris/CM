a8afdb974c8cc24e92b3ee1a0f726f9d9f7c4053
apobekiaris <apostolis.bekiaris@gmail.com>
2023-12-07 12:45:57 +0200

    fix errors

--- Start.ps1
+++ Start.ps1
@@ -4,11 +4,21 @@ param(
     $DXApiFeed=$env:DxFeed
 )
 $url = "https://$GithubUserName`:$GithubPass@github.com/apobekiaris/CryptoManager2.git"
-git clone $url
+if (!(Test-Path .\CryptoManager2)){
+  git clone $url
+}
+else{
+  Push-Location .\CryptoManager2
+  git pull
+  Pop-Location
+}
 cd .\CryptoManager2
+if (!(dotnet nuget list source |sls DX)){
+  dotnet nuget add source $DXApiFeed --name DX
+  dotnet nuget add source https://xpandnugetserver.azurewebsites.net/nuget --name Xpand
+}
 
-dotnet nuget add source $DXApiFeed --name DX
-dotnet nuget add source https://xpandnugetserver.azurewebsites.net/nuget --name Xpand
+
 dotnet build --configuration Debug "/p:TestConstants=TEST"
 
 if ($LASTEXITCODE -ne 0) {
